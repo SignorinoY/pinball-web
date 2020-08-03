6b2ead4ad8baad74ed98d3242916fedf312b6e73
SignorinoY <meetziyang@gmail.com>
2020-08-03 14:21:43 +0800

    :sparkles: add share scene

--- src/scenes/ShareScene.ts
+++ src/scenes/ShareScene.ts
@@ -2,10 +2,31 @@ import cookie from 'cookiejs';
 
 import request = require('superagent');
 
+import Canvas from 'phaser3-rex-plugins/plugins/canvas.js';
+
+function ConvertAvatar2Base64(url, callback) {
+    var image = new Image();
+    image.crossOrigin = 'annoymous';
+    image.src = url;
+
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+
+    image.onload = function () {
+        canvas.height = 100;
+        canvas.width = 100;
+        ctx.drawImage(image, 0, 0, 100, 100);
+        var dataURL = canvas.toDataURL("image/");
+        callback.call(this, dataURL);
+        canvas = null;
+    };
+}
+
 export class ShareScene extends Phaser.Scene {
 
-    private player_text: Phaser.GameObjects.Text;
-    private player_photo: Phaser.GameObjects.Image;
+    private nickname: Phaser.GameObjects.Text;
+    private avatar: Phaser.GameObjects.GameObject;
+
     private score_text: Phaser.GameObjects.BitmapText;
     private rank_text: Phaser.GameObjects.BitmapText;
 
@@ -13,12 +34,10 @@ export class ShareScene extends Phaser.Scene {
     private rank: number = 100;
     private count: number = 1000;
 
-    private nickname: string;
-    private headimgurl: string;
-
     constructor() {
         super({ key: 'ShareScene' })
     }
+
 
     init(data): void {
         this.score = data['score'];
@@ -28,45 +47,36 @@ export class ShareScene extends Phaser.Scene {
 
     create(): void {
 
-        const openid = "ogIOb0RchliWDAylJW9FGLEwhlEI";
+        const openid = cookie.get('openid') as unknown;
 
+        var scene = this;
         if (openid != false) {
-            request.get('https://xwfintech.qingke.io/5ef21525813260002d508321/api/user/' + openid)
+            request
+                .get('https://xwfintech.qingke.io/5ef21525813260002d508321/api/user/' + openid)
                 .end((error, result) => {
-                    this.nickname = result.body.nickname;
-                    this.headimgurl = result.body.headimgurl;
+                    var canvas = new Canvas(this, 200, 200, 100, 100);
+                    ConvertAvatar2Base64(
+                        result.body.headimgurl,
+                        (base64) => {
+                            canvas.loadFromURLPromise(base64)
+                                .then(function () {
+                                    canvas.generateTexture('avatar');
+                                });
+                        })
+                    scene.avatar = scene.add.existing(canvas);
                 });
+        } else {
+            this.avatar = this.add.image(200, 200, 'default');
         }
-
-        this.nickname = this.nickname === undefined ? 'Unknown' : this.nickname;
-
-        var avatar = new Image();
-        avatar.src = this.headimgurl
-        if (this.headimgurl != '') {
-            this.load.image('player-photo', this.headimgurl);
-        }
-
-        const player_photo_texture = this.headimgurl == '' ? 'default' : 'player-photo';
 
         this.add.image(0, 0, 'background').setOrigin(0, 0).setDisplaySize(640, 960);
 
-        this.add.image(320, 240, 'trophy');
+        this.add.image(320, 240, 'title');
 
-        this.player_photo = this.add.image(320, 460, player_photo_texture);
-        this.player_photo.displayHeight = 100;
-        this.player_photo.displayWidth = 100;
+        this.score_text = this.add.bitmapText(300, 490, 'gem', 'Score: ' + this.score, 36, 1);
+        this.rank_text = this.add.bitmapText(300, 530, 'gem', 'Rank: ' + this.rank + '/' + this.count, 24, 1);
 
-        this.player_text = this.add.text(0, 520, this.nickname, { fontSize: '24px', fontStyle: 'bold' });
-        this.player_text.setX((640 - this.player_text.width) / 2);
-
-        this.score_text = this.add.bitmapText(0, 580, 'gem', 'Score: ' + this.score, 36, 1);
-        this.score_text.setX((640 - this.score_text.width) / 2);
-        this.rank_text = this.add.bitmapText(0, 620, 'gem', 'Rank: ' + this.rank + '/' + this.count, 24, 1);
-        this.rank_text.setX((640 - this.rank_text.width) / 2);
-
-        this.add.image(320, 780, 'QR-Code');
-
-        var canvas = document.getElementsByTagName("canvas")[0];
-        var png = canvas.toDataURL("image/png");
+        this.add.image(320, 720, 'QR-Code');
+        this.add.bitmapText(158, 840, 'gem', 'Press and hold the QR code,\n Let\'s challange together!', 24, 1);
     }
 }
