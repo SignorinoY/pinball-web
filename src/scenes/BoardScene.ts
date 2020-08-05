8349f2286f649e9cb1478a7ab89c83b5685269ef
SignorinoY <meetziyang@gmail.com>
2020-08-05 18:08:55 +0800

    :construction: leaderboard scene work in

--- src/scenes/BoardScene.ts
+++ src/scenes/BoardScene.ts
@@ -1,14 +1,65 @@
-import { GridTable } from 'phaser3-rex-plugins/templates/ui/ui-components.js';
+import request = require('superagent');
 
-const Random = Phaser.Math.Between;
-var CreateItems = function (count) {
-    var data = [];
-    for (var i = 0; i < count; i++) {
-        data.push({
-            id: i,
-            color: Random(0, 0xffffff)
-        });
+import { GridTable, Sizer, Canvas } from 'phaser3-rex-plugins/templates/ui/ui-components.js';
+
+function ConvertAvatar2Base64(url) {
+    return new Promise((resolve, reject) => {
+        var image = new Image();
+        image.crossOrigin = 'annoymous';
+        image.src = url;
+        var canvas = document.createElement("canvas");
+        canvas.height = 100;
+        canvas.width = 100;
+        var ctx = canvas.getContext("2d");
+        image.onload = () => {
+            ctx.drawImage(image, 0, 0, 100, 100);
+            resolve(canvas.toDataURL("image/"));
+            canvas = null;
+        }
+    });
+}
+
+const GetValue = Phaser.Utils.Objects.GetValue;
+
+var CreateRowItem = function (scene, config) {
+    var id = GetValue(config, 'id', undefined);
+    if (id === undefined) {
+        id = scene.add.bitmapText(0, 0, 'gem', id, 36);
     }
+    var avatar = GetValue(config, 'avatar', undefined);
+    if (avatar === undefined) {
+        avatar = scene.add.existing(new Canvas(scene, 0, 0, 100, 100));
+    }
+    var nickname = GetValue(config, 'nickname', undefined);
+    if (nickname === undefined) {
+        nickname = scene.add.text(0, 0, nickname);
+    }
+    var score = GetValue(config, 'score', undefined);
+    if (score === undefined) {
+        score = scene.add.bitmapText(0, 0, 'gem', score, 36);
+    }
+    return scene.add.existing(
+        new Sizer(scene, {
+            width: GetValue(config, 'width', undefined),
+            height: GetValue(config, 'height', undefined),
+            orientation: 'x',
+        })
+            .add(id, 0, 'center', { left: 10 }, false, 'id')
+            .addSpace()
+            .add(avatar, 0, 'center', {}, false, 'avatar')
+            .addSpace()
+            .add(nickname, 0, 'center', {}, false, 'nickname')
+            .addSpace()
+            .add(score, 0, 'center', {}, false, 'score')
+    );
+}
+
+async function getItems(count) {
+    let data;
+    data = await request
+        .get('https://xwfintech.qingke.io/5ef21525813260002d508321/api/rank')
+        .query({ limit: count });
+    console.log(data)
     return data;
 }
 
@@ -19,24 +70,52 @@ export class BoardScene extends Phaser.Scene {
 
     create() {
         this.add.image(0, 0, 'background').setOrigin(0, 0).setDisplaySize(640, 960);
+
+        this.add.bitmapText(176, 120, 'gem', 'Leader Board', 48)
         var scrollMode = 0; // 0:vertical, 1:horizontal
         var table = new GridTable(this, {
-            x: 400,
-            y: 300,
-            width: (scrollMode === 0) ? 300 : 420,
-            height: (scrollMode === 0) ? 420 : 300,
-            scrollMode: scrollMode,
+            x: 320,
+            y: 480,
+            width: 480,
+            height: 640,
+
+            scroolMode: 0,
+
             table: {
-                cellWidth: (scrollMode === 0) ? undefined : 60,
-                cellHeight: (scrollMode === 0) ? 60 : undefined,
-                columns: 2,
-                mask: {
-                    padding: 2,
-                },
+                cellWidth: undefined,
+                cellHeight: 60,
+                columns: 1,
                 reuseCellContainer: true,
             },
-            items: CreateItems(100)
-        });
+            space: {
+                left: 20,
+                right: 20,
+                top: 20,
+                bottom: 20,
+                table: 20,
+                header: 10,
+                fotter: 10,
+            },
+            createCellContainerCallback: function (cell, cellContainer) {
+                var scene = cell.scene,
+                    width = cell.width,
+                    height = cell.height,
+                    item = cell.item,
+                    index = cell.index;
+                if (cellContainer === null) {
+                    var score = scene.add.bitmapText(0, 0, 'gem', '', 36);
+                    cellContainer = CreateRowItem(scene, undefined);
+                }
+                // Set properties from item value
+                cellContainer.setMinSize(width, height); // Size might changed in this demo
+                cellContainer.getElement('id').setText(item.id);
+                cellContainer.getElement('nickname').setText(item.nickname);
+                cellContainer.getElement('score').setText(item.score);
+                return cellContainer;
+            },
+            items: getItems()
+        })
+            .layout();
         this.add.existing(table);
     }
 
