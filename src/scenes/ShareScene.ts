e2170eb6c3fed5d87dda0ae0ca229dd7c0ad8d31
SignorinoY <meetziyang@gmail.com>
2020-08-06 16:33:09 +0800

    :bug: fix share scene avatar load bug

--- src/scenes/ShareScene.ts
+++ src/scenes/ShareScene.ts
@@ -13,7 +13,7 @@ async function addUserInfo(scene, openid) {
         const base64 = await convertAvatar2Base64(result.body.headimgurl);
         if (base64 != '') {
             scene.add.existing(
-                new Canvas(this, 220, 520, 100, 100).loadFromURLPromise(base64)
+                new Canvas(scene, 220, 520, 100, 100).loadFromURLPromise(base64)
             );
         }
     } else {
